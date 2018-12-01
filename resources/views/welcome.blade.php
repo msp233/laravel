67f247483a5bfc9a034ba73948eb00ada4f016de
msp <msp@mspdeMBP.lan>
2018-12-01 15:28:17 +0800

    change views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,12 +85,13 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
+                     laravel第一课
+                    {{--<a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>--}}
                 </div>
             </div>
         </div>
